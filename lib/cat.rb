b0a618b9a3d4e26f68e3a75f0cb9f6801eb5f43c
Jackson Reynolds <jwreynold@gmail.com>
2020-07-20 20:21:05 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
+require 'owner.rb'
+
 class Cat
-  # code goes here
+  attr_accessor :mood, :owner
+  attr_reader :name
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = 'nervous'
+    @@all << self
+  end #initialize
+
+  def self.all
+    @@all
+  end #self.all
 end
