29562a38de566eaefb994e854e15b14ca20658bc
Doug Schallmoser <dougschallmoser@mgail.com>
2020-07-20 18:28:33 -0700

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,71 @@
+## An owner will know about all their pets
+##    -Can buy a pet
+##    -Change a pet's mood
+##    -Sell all their pets
+
 class Owner
-  # code goes here
+  
+  attr_reader :name, :species
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end 
+
+  def species(species = "human")
+    @species = species 
+  end 
+
+  def say_species 
+    "I am a #{self.species}."
+  end 
+
+  def self.all 
+    @@all 
+  end 
+
+  def self.count 
+    self.all.count
+  end 
+
+  def self.reset_all
+    self.all.clear
+  end 
+
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end 
+
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end 
+
+  def buy_cat(name)
+    cat = Cat.new(name, self)
+  end 
+
+  def buy_dog(name)
+    dog = Dog.new(name, self)
+  end 
+
+  def walk_dogs 
+    Dog.all.select {|dog| dog.mood = "happy"}
+  end 
+
+  def feed_cats
+    Cat.all.select {|cat| cat.mood = "happy"}
+  end 
+
+  def sell_pets
+    Dog.all.select {|dog| dog.mood = "nervous"}
+    Dog.all.select {|dog| dog.owner = nil}
+    Cat.all.select {|cat| cat.mood = "nervous"}
+    Cat.all.select {|cat| cat.owner = nil}
+  end 
+
+  def list_pets 
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end 
+
 end
